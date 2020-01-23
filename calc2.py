a482a231557622d50fc8b6892984dcc203fec944
briantrimedx <57999883+briantrimedx@users.noreply.github.com>
2020-01-23 11:51:12 -0500

    Add files via upload

---
+++ calc2.py
@@ -0,0 +1,5 @@
+x = int(input('Enter the first number: '))
+y = int(input('Enter the second number: '))
+result = x * y
+#cONVERTS THE INTEGERS INTO STRINGS 
+print(str(x) + " * " + str(y) + " = " + str(result))
